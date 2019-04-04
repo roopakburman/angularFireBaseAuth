f38d1f478203ac0fd2b5da3d943280608d350fa4
Roopak Burman <roopak.burman@gmail.com>
2019-04-04 12:35:27 +0530

    first commit

---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.css']
+})
+export class InfoComponent implements OnInit {
+
+  constructor(
+    public router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBziagVMDqnGN8TrcxkpjVlQj3GqfHliMg",
+    authDomain: "angularfirebaseauth-d388a.firebaseapp.com",
+    databaseURL: "https://angularfirebaseauth-d388a.firebaseio.com/users",
+    projectId: "angularfirebaseauth-d388a",
+    storageBucket: "angularfirebaseauth-d388a.appspot.com",
+    messagingSenderId: "538677574734"
+  }
+};
